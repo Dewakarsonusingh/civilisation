4a0711795743d0890f2fabadf4fcad7ec46f9d6d
cha014 <c-h-F-15@kcs.org.uk>
2016-05-18 19:27:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function getResource() {
   resource += 1;
   document.getElementById("resourceNum").innerHTML = resource; 
 }
+
+var money = 0;
+
+function getSell() {
+  resource *= 1.5;
+  document.getElementById("moneyNum").innerHTML = resource;
+}
