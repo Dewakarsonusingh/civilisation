bccc4c608701a7c0edc298603399c3e65a6ccccb
cha014 <c-h-F-15@kcs.org.uk>
2016-05-18 18:45:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+var resource = 0;
 
+
+function buttonAlert() {
+  alert("Hey, I'm a cringey alert!");
+}
+
+function getRock() {
+  resource += 1;
+  document.getElementById("resourceNum").innerHTML = resource; 
+}
