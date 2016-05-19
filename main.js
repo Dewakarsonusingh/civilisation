8a87eb0ad344668abf3112ad868d0197858ab149
cha014 <c-h-F-15@kcs.org.uk>
2016-05-19 17:33:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,10 @@ function getSell() {
   document.getElementById("moneyNum").innerHTML = money;
   document.getElementById("resourceNum").innerHTML = resource;
 }
+function getEmploy() {
+  money -= 100;
+}
+
+if (money < 0) {
+   alert("The flame of civilisation has gone out!");
+}
